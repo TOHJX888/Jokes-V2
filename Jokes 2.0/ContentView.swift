9dad8d1a1caed739806864067b1cf912f724b7e4
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2024-04-10 19:33:50 +0800

    did the layout

--- Jokes 2.0/ContentView.swift
+++ Jokes 2.0/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var jokes = [Joke(setup: "Why couldn't the bicycle stand up?", punchline: "It was two tired!"), Joke(setup: <#T##String#>, punchline: <#T##String#>)]
+    var jokes =
+    [Joke(setup: "Why couldn't the bicycle stand up?", punchline: "It was two tired!"),
+     Joke(setup: "Why did the chicken cross the road?", punchline: "To get to the other side!"),
+    ]
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(jokes[0].setup)
+                .padding()
+            
+            Button {
+                print("Button tapped!")
+            } label: {
+                Text("What?")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            
+            Text(jokes[0].punchline)
+                .padding()
+            
+            Text("Tap to continue")
+                .italic()
+                .padding()
         }
-        .padding()
     }
 }
 
