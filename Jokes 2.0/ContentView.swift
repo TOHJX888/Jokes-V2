e2f7decd53038a571271414867c7f7e66ebacad4
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2024-04-10 19:11:19 +0800

    Initial Commit

---
+++ Jokes 2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Jokes 2.0
+//
+//  Created by RGS on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jokes 2.0/Jokes_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_2_0App.swift
+//  Jokes 2.0
+//
+//  Created by RGS on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
